4eaead7d13385605f60bfce0cd99347a86707738
aord9807013750 <ajailinuxgaurav@gmail.com>
2020-05-07 17:58:29 +0530

    first commit

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Aord and evlab ready to fly',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
